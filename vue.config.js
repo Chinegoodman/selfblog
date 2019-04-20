647c337f3c5aaa3b00435ddda3a3a95eb4b24964
pangshifeng <pangshifeng0539@126.com>
2019-04-21 00:53:14 +0800

    备注了端口设置，只留有代理设置部分。方便在项目启动后 直接看到默认输出的network 的ip地址，方便后期项目在移动端调试。如后期有bug则在把备注取消

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     publicPath:'./',
     devServer: {
-        open: false,
-        host: 'localhost',
-        port: 8080,
-        https: false,
+        // open: false,
+        // host: 'localhost',
+        // port: 8080,
+        // https: false,
         proxy: {
             '/juheapi': {
                 target: 'http://v.juhe.cn',
